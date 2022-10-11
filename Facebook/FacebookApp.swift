fc4c344e9545fb3417a45e5e53f8a83c6e5c699b
Jessica Sendejo <jsendejo1@mail.sfsu.edu>
2022-10-11 13:46:00 -0700

    Initial Commit

---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Jessica Sendejo on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
